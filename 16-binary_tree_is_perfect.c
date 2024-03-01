98b4c9d27bca17f00cf2779a1210fffde408096a
Ifaol <ifaolhunde@gmail.com>
2024-03-01 06:11:01 -0800

    A function that checks if a binary tree is perfect updated

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-int left = 1, right = 1;
+int left = 0, right = 0;
 if (tree != NULL)
 {
 if (tree->right)
-right = right + binary_tree_height(tree->right);
+right = 1 + binary_tree_height(tree->right);
 if (tree->left)
-left = left + binary_tree_height(tree->left);
+left = 1 + binary_tree_height(tree->left);
 if (left > right)
 {
 return (left);
